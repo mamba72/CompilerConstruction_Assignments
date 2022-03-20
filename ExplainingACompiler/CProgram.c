b9e93da83dea50310e3bf71d16905339df368b74
Stephen White <44649115+mamba72@users.noreply.github.com>
2022-03-20 11:08:12 -0700

    Created C factorial and Assembly

---
+++ ExplainingACompiler/CProgram.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int num = 5;
+
+    int product = 1;
+
+    while(num > 0)
+    {
+        product *= num;
+
+        num--;
+    }
+
+    printf(product);
+
+    return 0;
+}
