f4188a2b3b5bfdcb061fc5c497b27c5f144d4097
Stephen White <44649115+mamba72@users.noreply.github.com>
2022-05-16 16:15:47 -0700

    did lots of stuff. changed many things

---
+++ ExplainingACompiler/CProgram_Easy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+//Created a program does factorial
+int main()
+{
+    int num = 5;
+
+    // int product = 1;
+
+    // while(num > 0)
+    // {
+    //     product *= num;
+
+    //     num--;
+    // }
+
+    // printf(product);
+
+    return 0;
+}
